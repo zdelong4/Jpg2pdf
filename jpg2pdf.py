5f74529a5cda01dc40a1e591e4abb1b531c51d93
Zach DeLong <zdelong4@gmail.com>
2024-10-24 17:37:50 -0500

    Added support for folder input
    The user may now place a folder of jpg files into the jpg folder. This feature was added to to allow for zip files to be extracted directly into the jpg folder.

--- jpg2pdf.py
+++ jpg2pdf.py
@@ -1,24 +1,47 @@
 import os
 import img2pdf
 
-dir_jpg = 'jpg'
-print("Booting up JPG CONVERTER 9000...")
-for filename in os.listdir("jpg"):
-    if(filename != ".DS_Store"):
-        print("jpg/" + filename)
-        input = "jpg/" + filename
-        if "jpg" in filename:
-            output = "pdf/" + filename.replace("jpg", "pdf")
-        elif "jpeg" in filename: 
-            output = "pdf/" + filename.replace("jpeg", "pdf")
-        else :
-            output = "pdf/" + filename
-        with open(input, "rb") as f:
-            pdf_bytes = img2pdf.convert(f.read())
-        with open(output, "wb") as f:
-            f.write(pdf_bytes) 
-        # os.remove(input)
-        print(filename + " processed...")
-# print("JPG CONVERTER 9000 done. .../jpg folder cleaned")
-
-    
+def main():
+    dir_jpg = 'jpg'
+    print("Booting up JPG CONVERTER 9000...")
+    for filename in os.listdir("jpg"):
+        # macOS creates temporary files in directories, avoid these
+        if(filename != ".DS_Store"):
+            # Check for folder input
+            print(filename + " " + str(os.path.isdir(filename)))
+            # print(os.path.isdir(filename))
+            if(os.path.isdir(filename)):
+                print("here")
+                for file in os.listdir(filename):
+                    # check for temporary file again when traversing the new directory
+                    if(filename != ".DS_Store"):
+                        input = "jpg/" + filename
+                        if "jpg" in filename:
+                            output = "pdf/" + filename.replace("jpg", "pdf")
+                        elif "jpeg" in filename: 
+                            output = "pdf/" + filename.replace("jpeg", "pdf")
+                        else:
+                            output = "pdf/" + filename
+                            break
+                        with open(input, "rb") as f:
+                            pdf_bytes = img2pdf.convert(f.read())
+                        with open(output, "wb") as f:
+                            f.write(pdf_bytes) 
+                        # os.remove(input)
+                        print(filename + "processed...")
+            else:
+                input = "jpg/" + filename
+                if "jpg" in filename:
+                    output = "pdf/" + filename.replace("jpg", "pdf")
+                elif "jpeg" in filename: 
+                    output = "pdf/" + filename.replace("jpeg", "pdf")
+                else :
+                    output = "pdf/" + filename
+                    break
+                with open(input, "rb") as f:
+                    pdf_bytes = img2pdf.convert(f.read())
+                with open(output, "wb") as f:
+                    f.write(pdf_bytes) 
+                # os.remove(input)
+                print(filename + "processed...")
+    print("JPG CONVERTER 9000 done. .../jpg folder cleaned")
